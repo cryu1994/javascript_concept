08e9f3fcabef740299bcb8a70669c36e259c2169
Home <Home@Homes-MacBook-Pro-2.local>
2017-03-27 14:04:34 -0700

    done

--- declare/index.js
+++ declare/index.js
@@ -3,7 +3,13 @@ function a(){
     console.log(string[0], string[1], string[2]);
     var int = [2, 4, 6, 8];
     console.log(int);
-    var undefined;
-    console.log(undefined);
+    var myVar;
+    console.log(myVar);
+    if(string[0] == string[1]){
+        return false;
+    }
+    else{
+        return true;
+    }
 }
 a();
