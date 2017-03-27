959813a651b10f939256c1ab91a71490f34aecf3
Home <Home@Homes-MacBook-Pro-2.local>
2017-03-27 13:58:26 -0700

    addingsome lines

---
+++ declare/index.js
@@ -0,0 +1,9 @@
+function a(){
+    var string = ["Hello","world", "!"];
+    console.log(string[0], string[1], string[2]);
+    var int = [2, 4, 6, 8];
+    console.log(int);
+    var undefined;
+    console.log(undefined);
+}
+a();
